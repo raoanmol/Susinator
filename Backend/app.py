9d665257d4c0a8d7214efb99787d52da1887a50a
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-11-04 15:14:18 -0700

    first commit

---
+++ Backend/app.py
@@ -0,0 +1,13 @@
+# Dowmloading dependencies for this project.
+# We recommend that you run the project in a virtual env
+
+import os, yaml
+
+# Install dependencies
+with open('config/package.yaml', 'r') as file:
+    s = yaml.safe_load(file)['dependencies']
+
+os.system('pip install ' + s)
+
+# Start the API server
+os.system("python server.py")
