8c3da6a57c70054ea575c34970d61d38676958af
Anmol Rao <raoanmol@outlook.com>
2023-11-05 01:10:52 -0700

    Fixed parser

--- Backend/dynamodb_to_s3.py
+++ Backend/dynamodb_to_s3.py
@@ -1,6 +1,16 @@
+# METHOD TO FETCH output.txt
+def fetch_output_from_s3(s3, s3_bucket, bucket_name):
+    try:
+        # Retrieve the contents of the file
+        response = s3.get_object(Bucket=bucket_name, Key="output.txt")
+        content = response['Body'].read().decode('utf-8')
+        return content
+    except Exception as e:
+        return str(e)
+    
 # METHOD TO FETCH summary.txt
 
-def fetch_summary_from_s3(s3, s3_bucket):
+def fetch_summary_from_s3(s3, s3_bucket, bucket_name):
     try:
         # Retrieve the contents of the file
         response = s3.get_object(Bucket=bucket_name, Key="summary.txt")
@@ -12,7 +22,7 @@ def fetch_summary_from_s3(s3, s3_bucket):
 
 # METHOD TO FETCH bindings.txt
 
-def fetch_binding_from_s3(s3, s3_bucket):
+def fetch_binding_from_s3(s3, s3_bucket, bucket_name):
     try:
         # Retrieve the contents of the file
         response = s3.get_object(Bucket=bucket_name, Key="bindings.txt")
--- Backend/upload_to_s3.py
+++ Backend/upload_to_s3.py
@@ -1,7 +1,28 @@
 import boto3
+from io import BytesIO
 
-
-def upload_file_to_s3(file_name, bucket_name):
+def upload_file_to_s3(file_data, bucket_name, file_name):
     # Create an S3 client
     s3 = boto3.client('s3')
-    s3.upload_file(file_name, bucket_name, file_name)
+    
+    # If file_data is a string, encode it to bytes
+    if isinstance(file_data, str):
+        file_data = file_data.encode()
+    
+    # If file_data is bytes, create a BytesIO object from it
+    if isinstance(file_data, bytes):
+        file_data = BytesIO(file_data)
+    
+    # Ensure file_data is now a BytesIO object
+    if not isinstance(file_data, BytesIO):
+        raise ValueError("file_data must be a string, bytes, or BytesIO object")
+    
+    # Reset the file_data BytesIO object's cursor to the start of the data
+    file_data.seek(0)
+    
+    # Use the put_object method to upload the data
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=file_data
+    )
