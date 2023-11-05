0365f289d6b86c04b0c3c7aa6a55bbf109678517
Anmol Rao <raoanmol@outlook.com>
2023-11-04 22:12:16 -0700

    Slight create_lambda refactor

--- Backend/create_lambda.py
+++ Backend/create_lambda.py
@@ -1,15 +1,22 @@
-# will have to manually create a IAM
-# ARN: arn:aws:iam::909768541970:role/sun-hacks-IAM
-
 import boto3
+import random
+import string
+
+def generate_random_string(length=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 # Initialize a session using Amazon Lambda
 session = boto3.session.Session(region_name='us-east-2')  # replace 'your-region' with your AWS region
 lambda_client = session.client('lambda')
 
+# Generate a random function name
+random_string = generate_random_string()
+function_name = f'sun-hacks-lambda-{random_string}'
+
 # Define the Lambda function
 response = lambda_client.create_function(
-    FunctionName='JobOfferSummaryFunction',
+    FunctionName=function_name,
     Runtime='python3.11',  # replace '3.x' with your Python version
     Role='arn:aws:iam::909768541970:role/sun-hacks-IAM',  # replace 'your-account-id' and 'execution_role' with your values
     Handler='lambda_function.lambda_handler',  # the file and function names from the zip file
