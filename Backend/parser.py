0800fa7f79c21444d3f30c7aa70a5d60375fe9e9
Anmol Rao <raoanmol@outlook.com>
2023-11-04 17:07:46 -0700

    Fixed parser

--- Backend/parser.py
+++ Backend/parser.py
@@ -1,35 +1,65 @@
 import boto3
 import time
 
-def extract_text(file_path):
-    # Initialize a session using Amazon Textract
-    textract = boto3.client('textract', region_name='us-east-1')
-    
-    # Read the file
-    with open(file_path, 'rb') as file:
-        file_bytes = bytearray(file.read())
-        print('Image loaded', file_path)
-    
-    # Call Amazon Textract
-    response = textract.start_document_text_detection(Document={'Bytes': file_bytes})
-    
-    # Get the job id from the response and wait for the job to complete
+# Initialize variables
+input_file_name = 'input_file_name'
+bucket_name = 'bucket_name'
+output_file_path = 'output_file_path'
+
+def upload_file_to_s3(file_name, bucket_name):
+    # Create an S3 client
+    s3 = boto3.client('s3')
+    s3.upload_file(file_name, bucket_name, file_name)
+
+def extract_text_from_pdf(bucket_name, file_name, output_file_path):
+    # Create a Textract client
+    textract = boto3.client('textract')
+
+    # Start document text detection
+    response = textract.start_document_text_detection(
+        DocumentLocation={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': file_name
+            }
+        }
+    )
+
+    # Get the job id from the response
     job_id = response['JobId']
-    print(f'Started job with id: {job_id}')
-    
+
+    # Wait for the job to complete
+    print(f'Waiting for job {job_id} to complete...')
+    status = ''
     while True:
         response = textract.get_document_text_detection(JobId=job_id)
         status = response['JobStatus']
-        print(f'Job status: {status}')
-        if status in ['SUCCEEDED', 'FAILED']:
+        if status == 'SUCCEEDED':
             break
-        time.sleep(5)  # wait for a while before checking the status again
+        elif status == 'FAILED':
+            print(f'Job {job_id} failed: {response["StatusMessage"]}')
+            return
+        print(f'Job status: {status}')
+        time.sleep(5)
 
-    # Print the text
-    if status == 'SUCCEEDED':
-        for page in response['Blocks']:
-            if page['BlockType'] == 'LINE':
-                print(page['Text'])
+    # Write the text to the output file
+    pages = []
+    next_token = None
+    while True:
+        response = textract.get_document_text_detection(JobId=job_id, NextToken=next_token) if next_token \
+            else textract.get_document_text_detection(JobId=job_id)
 
-# Call the function
-extract_text('D:\\Code\\SunHacks23\\Data\\letter.pdf')
+        pages.extend(response['Blocks'])
+
+        next_token = response.get('NextToken', None)
+        if not next_token:
+            break
+
+    with open(output_file_path, 'w') as f:
+        for block in pages:
+            if block['BlockType'] == 'LINE':
+                f.write(block['Text'] + '\n')
+
+# Usage:
+upload_file_to_s3(input_file_name, bucket_name)
+extract_text_from_pdf(bucket_name, input_file_name, output_file_path)
